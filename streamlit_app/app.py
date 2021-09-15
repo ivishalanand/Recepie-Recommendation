cc4d9798667beb83aa8846206c779b66931a2037
ivishalanand <vishal.ananad456@gmail.com>
2021-09-15 10:34:40 +0530

    added emojis

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -26,7 +26,7 @@ def get_data():
 
 try:
     df = get_data()
-    original_title = '<p style=color:#f63468; font-size:30px;">Choose Meals</p>'
+    original_title = '<p style="color:#f63468; font-size:25px";">Choose Meals</p>'
     st.markdown(original_title, unsafe_allow_html=True)
     meals = st.multiselect(
         "", list(df.Meals),
@@ -41,7 +41,7 @@ try:
     # with col2:
         # Create a simple button 
     if(st.button("Generate Recommendations")):
-        placeholder = st.markdown("This may take a bit longer the first time.........!!! ![Alt Text](https://media.giphy.com/media/9rgi4j5MclWdMNIsHz/giphy.gif)")
+        placeholder = st.markdown("![Alt Text](https://media.giphy.com/media/9rgi4j5MclWdMNIsHz/giphy.gif)")
         recommendations = get_recommendations(meals)
         placeholder.empty()
         placeholder.empty()
@@ -55,7 +55,9 @@ try:
         st.markdown(subtitle, unsafe_allow_html=True)
         for recommendation in recommendations:
             url = meal_url_mapper.loc[recommendation]["url"]
-            st.write("🍕" + recommendation + " [link](" + url + ")")
+            prep_time = meal_url_mapper.loc[recommendation]["prep_time"]
+            region = meal_url_mapper.loc[recommendation]["region"]
+            st.write("🍕" + recommendation + " ⏳ " + prep_time + " 🌎 " + region + " [link](" + url + ")")
             # st.write("check out this [link](url)")
             
 
