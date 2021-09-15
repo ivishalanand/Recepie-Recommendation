6be29ca4fe720df55b01662a012ea7c291f9f91f
ivishalanand <vishal.ananad456@gmail.com>
2021-09-15 19:20:13 +0530

    locally to be run on azure

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -1,21 +1,50 @@
-from requests.sessions import default_headers
-import streamlit as st
-import pandas as pd
-import requests
-import time
 from urllib.error import URLError
+
+import pandas as pd
+import streamlit as st
+
+
+def get_response_locally(meals):
+    similarity_matrix = pd.read_csv("/Users/visanand2/Desktop/Pycharm/Recepie-Recommendation/fastapi_azure/similarity_matrix.csv", index_col=0)
+    similarity_matrix.index.name = "MEALS"
+
+    if len(meals) == 1:
+        recco = list(similarity_matrix[meals[0]].sort_values(ascending=False)[1:11].keys())
+
+    if len(meals) == 2:
+        recco = similarity_matrix[meals[0]].sort_values(ascending=False)[1:6]
+        recco = list(
+            recco.append(similarity_matrix[meals[1]].sort_values(ascending=False)[1:6]).keys())
+
+    if len(meals) == 3:
+        recco = similarity_matrix[meals[0]].sort_values(ascending=False)[1:5]
+        recco = recco.append(similarity_matrix[meals[1]].sort_values(ascending=False)[1:4])
+        recco = list(
+            recco.append(similarity_matrix[meals[2]].sort_values(ascending=False)[1:4]).keys())
+
+    if len(meals) == 4:
+        recco = similarity_matrix[meals[0]].sort_values(ascending=False)[1:4]
+        recco = recco.append(similarity_matrix[meals[1]].sort_values(ascending=False)[1:4])
+        recco = recco.append(similarity_matrix[meals[2]].sort_values(ascending=False)[1:3])
+        recco = list(
+            recco.append(similarity_matrix[meals[3]].sort_values(ascending=False)[1:3]).keys())
+
+    if len(meals) == 5:
+        recco = similarity_matrix[meals[0]].sort_values(ascending=False)[1:3]
+        recco = recco.append(similarity_matrix[meals[1]].sort_values(ascending=False)[1:3])
+        recco = recco.append(similarity_matrix[meals[2]].sort_values(ascending=False)[1:3])
+        recco = recco.append(similarity_matrix[meals[3]].sort_values(ascending=False)[1:3])
+        recco = list(
+            recco.append(similarity_matrix[meals[4]].sort_values(ascending=False)[1:3]).keys())
+
+    return recco
 
 
 def get_recommendations(meals):
-    headers = {
-    'accept': 'application/json',
-    'Content-Type': 'application/json',
-    }
-    data = str({ "meals": meals }).replace("'", '^').replace('"', "'").replace('^','"')
+    response = get_response_locally(meals)
+    # response = requests.post('https://recommendation-recipe.azurewebsites.net/recipe-recommendation/', headers=headers, data=data)
 
-    response = requests.post('https://recommendation-recipe.azurewebsites.net/recipe-recommendation/', headers=headers, data=data)
-
-    return response.json()["recommended_items"]
+    return response
 
 
 @st.cache
@@ -30,17 +59,17 @@ try:
     st.markdown(original_title, unsafe_allow_html=True)
     meals = st.multiselect(
         "", list(df.Meals),
-         default=["Creamy King Prawn Linguine", "Paneer Butter Masala With Coriander Naan"]
+        default=["Creamy King Prawn Linguine", "Paneer Butter Masala With Coriander Naan"]
     )
     if not meals:
         st.error("Please select at least one meal.")
     if len(meals) > 5:
         st.error("Please enter a maximum of 5 meals")
-    
+
     # col1, col2, col3, col4, col5 = st.columns(5)
     # with col2:
-        # Create a simple button 
-    if(st.button("Generate Recommendations")):
+    # Create a simple button
+    if (st.button("Generate Recommendations")):
         placeholder = st.markdown("![Alt Text](https://media.giphy.com/media/9rgi4j5MclWdMNIsHz/giphy.gif)")
         recommendations = get_recommendations(meals)
         placeholder.empty()
@@ -49,7 +78,8 @@ try:
         st.markdown("")
         st.markdown("")
         st.markdown("")
-        meal_url_mapper = pd.read_csv("/Users/visanand2/Desktop/Pycharm/Recepie-Recommendation/streamlit_app/meal_url_mapper.csv")
+        meal_url_mapper = pd.read_csv(
+            "/Users/visanand2/Desktop/Pycharm/Recepie-Recommendation/streamlit_app/meal_url_mapper.csv")
         meal_url_mapper.set_index("meal", inplace=True)
         subtitle = '<p style=color:#f63468; font-size: 15px;">Recommendations</p>'
         st.markdown(subtitle, unsafe_allow_html=True)
@@ -59,10 +89,9 @@ try:
             region = meal_url_mapper.loc[recommendation]["region"]
             st.write("🍕" + recommendation + " ⏳ " + prep_time + " 🌎 " + region + " [link](" + url + ")")
             # st.write("check out this [link](url)")
-            
 
         # st.altair_chart(chart, use_container_width=True)
-except URLError as e: 
+except URLError as e:
     st.error(
         """
         **This demo requires internet access.**
